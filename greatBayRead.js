d9b3e62883cb3f69569542c33c362e6c30f9e9db
Brad Williams <jb.wms89@gmail.com>
2020-07-07 13:16:07 -0400

    added things

--- greatBayRead.js
+++ greatBayRead.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = requires("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -10,8 +11,8 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "",
-  database: "ice_creamDB"
+  password: "8912",
+  database: "great_bayDB"
 });
 
 connection.connect(function(err) {
@@ -20,18 +21,34 @@ connection.connect(function(err) {
   createProduct();
 });
 
-function createProduct() {
-  console.log("Inserting a new product...\n");
+function promptUser() {
+    return inquirer.prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "Would you like to bid on or post an item?",
+        choices: ["Bid on an Item", "Post an Item" ]
+      }]).then(function(choice) {
+          switch (choice.role)  {
+            case "Bid on an Item":
+                newBid();
+                break;
+            case "Post an Item":
+                postItem()
+                break;
+         
+            }
+        });
+
+
+function postCard() {
+  console.log("Posting a new card...\n");
   var query = connection.query(
-    "INSERT INTO products SET ?",
-    {
-      flavor: "Rocky Road",
-      price: 3.0,
-      quantity: 50
-    },
+    "INSERT INTO cards SET ?",
+    {},
     function(err, res) {
       if (err) throw err;
-      console.log(res.affectedRows + " product inserted!\n");
+      console.log(res.affectedRows + " card auction posted!\n");
       // Call updateProduct AFTER the INSERT completes
       updateProduct();
     }
@@ -41,7 +58,7 @@ function createProduct() {
   console.log(query.sql);
 }
 
-function updateProduct() {
+function updateBid() {
   console.log("Updating all Rocky Road quantities...\n");
   var query = connection.query(
     "UPDATE products SET ? WHERE ?",
@@ -65,21 +82,21 @@ function updateProduct() {
   console.log(query.sql);
 }
 
-function deleteProduct() {
-  console.log("Deleting all strawberry icecream...\n");
-  connection.query(
-    "DELETE FROM products WHERE ?",
-    {
-      flavor: "strawberry"
-    },
-    function(err, res) {
-      if (err) throw err;
-      console.log(res.affectedRows + " products deleted!\n");
-      // Call readProducts AFTER the DELETE completes
-      readProducts();
-    }
-  );
-}
+// function deleteProduct() {
+//   console.log("Deleting all strawberry icecream...\n");
+//   connection.query(
+//     "DELETE FROM products WHERE ?",
+//     {
+//       flavor: "strawberry"
+//     },
+//     function(err, res) {
+//       if (err) throw err;
+//       console.log(res.affectedRows + " products deleted!\n");
+//       // Call readProducts AFTER the DELETE completes
+//       readProducts();
+//     }
+//   );
+// }
 
 function readProducts() {
   console.log("Selecting all products...\n");
